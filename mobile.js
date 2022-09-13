8242a535bed6746baf558979292bb1ed865c80c1
Adebayo-George <olayinkageorge855@gmail.com>
2022-09-12 20:22:09 -0700

    Add mobile menu to mobile version portfolio

--- mobile.js
+++ mobile.js
@@ -1,6 +1,7 @@
 const bredcrumb = document.querySelector('#bredcrumb')
 const navBarMenu = document.querySelector('#mobile-menu')
 const xBtn = document.querySelector('.icon-wrapper')
+const listItems = document.querySelectorAll('.menu-list')
 
 bredcrumb.addEventListener('click', ()=> {
     bredcrumb.style.display = 'none'
@@ -12,3 +13,13 @@ xBtn.addEventListener('click', ()=> {
     navBarMenu.style.display = 'none'
     bredcrumb.style.display = 'block'
 })
+
+listItems.forEach((n) => (
+  n.addEventListener('click', () => {
+    xBtn.style.display = 'none'
+    navBarMenu.style.display = 'none'
+    bredcrumb.style.display = 'block'
+    })));
+
+
+
